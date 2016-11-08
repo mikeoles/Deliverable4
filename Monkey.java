b3510e214d7f80404380b6cfad2820718085d94e
Michael Oles <mbo10@pitt.edu>
2016-11-07 20:15:43 -0500

    Improve monkeysim

--- Monkey.java
+++ Monkey.java
@@ -5,7 +5,7 @@ public class Monkey {
     private static int _monkeyNum = 0;
 
     private int _thisMonkeyNum = 0;
-    
+
     private int _id = -1;
 
     private Banana _b = null;
@@ -14,16 +14,16 @@ public class Monkey {
      * Get this monkey's number
      * @return int monkey number
      */
-    
+
     public int getMonkeyNum() {
 	return _thisMonkeyNum;
     }
-    
+
     /**
      * Getter for id.
      * @return id of monkey
      */
-    
+
     public int getId() throws NoIdException {
 	if (_id < 0) {
 	    throw new NoIdException();
@@ -49,7 +49,7 @@ public class Monkey {
      * Checks to see if this monkey has a banana
      * @return true if has banana, false otherwise
      */
-    
+
     public boolean hasBanana() {
 	return _b != null;
     }
@@ -58,55 +58,44 @@ public class Monkey {
      * Receive a banana from another monkey
      * @param b - Banana given to this monkey
      */
-    
+
     public void throwBananaTo(Banana b) {
 	_b = b;
     }
 
     /**
-     * 
+     *
      * @return Banana - the banana this monkey held
      */
-    
+
     public Banana throwBananaFrom() {
 	Banana toReturn = _b;
 	_b = null;
 	return toReturn;
     }
-    
+
     /**
      * Generate a unique ID for this monkey.
-     * Note that monkey ID generation must 
+     * Note that monkey ID generation must
      * always return the correct value for
      * a given n (i.e., the id for the first
      * monkey should always be the same).
      * @param int n - monkey number
      * @return int - id for this monkey
      */
-    
-    public int generateId(int n) {
-	int toReturn = 100;
-	int bird = -900;
-	for (int l=0; l <1000; l++) {
-	    for (int m = 0; m < 100; m++) {
-		bird += Math.round(Math.atan(l + m));
-		bird -= Math.round(Math.sin(m * m)) * Math.exp(m * m);
-		toReturn += bird;
-	    }
-	}
-	toReturn += 10;
-	toReturn += n;
-	return toReturn;
-    }
+
+     public int generateId(int n) {
+       return n + 223492;
+     }
 
     /**
      * Monkey constructor
      */
-    
+
     public Monkey() {
 	_thisMonkeyNum = _monkeyNum;
 	_monkeyNum++;
 	_id = generateId(_thisMonkeyNum);
     }
-    
+
 }
--- MonkeySim.java
+++ MonkeySim.java
@@ -5,7 +5,7 @@ public class MonkeySim {
     private static List<Monkey> _monkeyList = new LinkedList<Monkey>();
 
     public static final int HEADER = 50000;
-    
+
     /**
      * Print out use message and exit with
      * error code 1.
@@ -26,11 +26,11 @@ public class MonkeySim {
      * @param args - array of args from command line
      * @return int - starting monkey
      */
-    
+
     public static int getStartingMonkeyNum(String[] args) {
-	
+
 	int s = 0;
-	
+
 	if (args.length != 1) {
 	    errorAndExit();
 	}
@@ -46,29 +46,16 @@ public class MonkeySim {
 	}
 
 	return s;
-	
+
     }
 
     /**
      * Get a reference to the first monkey in the list.
      * @return Monkey first monkey in list
      */
-    
-    public static Monkey getFirstMonkey(List<Monkey> ml) {
 
-	int x = ml.size() - 1;
-	int f = x * 33;
-	int r = 17;
-	for (int j = x; j >= 0; j--) {
-	    if (ml.get(j).getMonkeyNum() == 1 && f > 0) {
-		f--;
-		j = x;
-	    } else if (ml.get(j).getMonkeyNum() == 1 && f == 0) {
-		return ml.get(j);
-	    } 
-	}
-	
-	return null;
+    public static Monkey getFirstMonkey(List<Monkey> ml) {
+		    return ml.get(1);
     }
 
     /**
@@ -78,13 +65,10 @@ public class MonkeySim {
      * @param m2 Monkey thrown to
      * @return String string version of round
      */
-    
+
     public static String stringifyResults(int c, Monkey m, Monkey m2) {
 	String toReturn = new String("");
 	try {
-	    for (int j=0; j < HEADER; j++) {
-		toReturn += "@";
-	    }
 	    toReturn += new String("//Round ");
 	    toReturn += new String("" + c);
 	    toReturn += new String(": Threw banana from Monkey (#");
@@ -95,15 +79,15 @@ public class MonkeySim {
 	    System.out.println("INVALID MONKEY!");
 	    System.exit(2);
 	}
-	return toReturn.substring(HEADER);
+	return toReturn;
     }
-    
+
     /**
      * Return the number of the monkey with a banana
-     * @param 
+     * @param
      * @return int number of monkey w/ banana
      */
-    
+
     public static int monkeyWithBanana(List<Monkey> ml) {
 	for (int j=0; j < ml.size(); j++) {
 	    Monkey m = ml.get(j);
@@ -119,7 +103,7 @@ public class MonkeySim {
 	    }
 	}
 	return -1;
-		
+
     }
 
     public static int addMoreMonkeys(int n, List<Monkey> ml) {
@@ -144,10 +128,10 @@ public class MonkeySim {
      * @param mw watcher of monkey
      * @return int number of rounds taken to get to first monkey
      */
-    
+
     public static int runSimulation(List<Monkey> ml, MonkeyWatcher mw) {
 	int nextMonkey = -1;
-	
+
 	while (!getFirstMonkey(ml).hasBanana()) {
 	    mw.incrementRounds();
 	    Monkey m = ml.get(monkeyWithBanana(ml));
@@ -161,27 +145,27 @@ public class MonkeySim {
 	System.out.println("First monkey has the banana!");
 	return mw.getRounds();
     }
-    
+
     /**
      * Entry point of program - run MonkeySim.
      * Accepts one argument, the starting monkey
      * number.
      * @param args - Array of arguments from cmd line
      */
-    
+
     public static void main(String[] args) {
 
 	int s = getStartingMonkeyNum(args);
 	Monkey tmpMonkey;
 	Banana b = new Banana();
 	MonkeyWatcher mw = new MonkeyWatcher();
-	
+
 	for (int j = 0; j < s + 1; j++) {
 	    tmpMonkey = new Monkey();
 	    _monkeyList.add(tmpMonkey);
 	}
 	_monkeyList.get(s).throwBananaTo(b);
-	
+
 	int numRounds = runSimulation(_monkeyList, mw);
 	System.out.println("Completed in " + numRounds + " rounds.");
     }
