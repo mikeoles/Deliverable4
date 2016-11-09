bd520a7e55116bd2cfa011767ee7985758822f71
Nicholas Taglianetti <Nicholas Taglianetti>
2016-11-09 12:32:47 -0500

    tests added for getFirstMonkey but they do not pass

--- MonkeySimTest.java
+++ MonkeySimTest.java
@@ -1,3 +1,4 @@
+
 /**
  * CS1632 Deliverable 4
  *
@@ -7,7 +8,41 @@
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.*;
 
 public class MonkeySimTest {
 
+    @Test()
+	public void getFirstMonkeyReturnsMonkeyForNonEmptyList() {
+        List<Monkey> monkeyList = new LinkedList<Monkey>();
+        Monkey m = new Monkey();
+        monkeyList.add(m);
+        assertEquals(MonkeySim.getFirstMonkey(monkeyList), m);
+    }
+
+    @Test()
+	public void getFirstMonkeyReturnsNullForEmptyList() {
+        List<Monkey> monkeyList = new LinkedList<Monkey>();
+        assertNull(MonkeySim.getFirstMonkey(monkeyList));
+    }
+
+    @Test()
+	public void getFirstMonkeyNeverReturnsNotFirstMonkey() {
+        List<Monkey> monkeyList = new LinkedList<Monkey>();
+        Monkey m1 = new Monkey();
+        monkeyList.add(m1);
+        Monkey m2 = new Monkey();
+        monkeyList.add(m2);
+        Monkey m3 = new Monkey();
+        monkeyList.add(m3);
+        assertNotEquals(MonkeySim.getFirstMonkey(monkeyList), m2);
+        assertNotEquals(MonkeySim.getFirstMonkey(monkeyList), m3);
+    }
+
+    @Test()
+	public void getFirstMonkeyNeverReturnsMonkeyNotInList() {
+        List<Monkey> monkeyList = new LinkedList<Monkey>();
+        Monkey m = new Monkey();
+        assertNotEquals(MonkeySim.getFirstMonkey(monkeyList), m);
+    }
 }
