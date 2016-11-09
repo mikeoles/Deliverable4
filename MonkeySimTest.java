84af4f75f568f78d45150c9d21a76b3a8b570f0a
Michael Oles <mbo10@pitt.edu>
2016-11-09 16:31:12 -0500

    Add Tests

--- MonkeySimTest.java
+++ MonkeySimTest.java
@@ -9,6 +9,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.*;
+import org.mockito.*;
 
 public class MonkeySimTest {
 
@@ -45,4 +46,40 @@ public class MonkeySimTest {
         Monkey m = new Monkey();
         assertNotEquals(MonkeySim.getFirstMonkey(monkeyList), m);
     }
+
+    //Test that if a Monkey with a negative id is passed into stringifyResults
+    //That the method catches the error and exits running
+    @Test()
+    public void testNegavtiveIdStringify()throws NoIdException{
+      Monkey m = Mockito.mock(Monkey.class);
+      Monkey m2 = new Monkey();
+      Mockito.when(m.getId()).thenThrow(NoIdException.class);
+      MonkeySim.stringifyResults(2,m,m2);
+      //program should exit after this line
+      fail();
+    }
+
+  //Test that stringifyResults returns a string with
+  //the correct number of the monkey that threw and caught a banana
+  @Test()
+  public void testMonkeyNums() {
+    Monkey m = Mockito.mock(Monkey.class);
+    Mockito.when(m.getMonkeyNum()).thenReturn(4);
+    Monkey m2 = Mockito.mock(Monkey.class);
+    Mockito.when(m2.getMonkeyNum()).thenReturn(1000);
+    String s = MonkeySim.stringifyResults(8,m,m2);
+    assertTrue(s.contains("Threw banana from Monkey (#4")&&s.contains("to Monkey (#1000"));
+  }
+
+  //Test the stringifyResults returns a string with
+  //the correct round based on the first parameter
+  @Test()
+  public void testCorrectRound() {
+    Monkey m = Mockito.mock(Monkey.class);
+    Mockito.when(m.getMonkeyNum()).thenReturn(2);
+    Monkey m2 = Mockito.mock(Monkey.class);
+    Mockito.when(m2.getMonkeyNum()).thenReturn(3);
+    String s = MonkeySim.stringifyResults(27,m,m2);
+    assertTrue(s.contains("//Round 27"));
+  }
 }
--- MonkeyTest.java
+++ MonkeyTest.java
@@ -9,5 +9,25 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonkeyTest {
+  @Test
+  public void returnsCorrectId(){
+    Monkey m = new Monkey();
+    int id = m.generateId(3);
+    assertEquals(id,223495);
+  }
 
+  @Test
+  public void checkSameIdReturned(){
+    Monkey m = new Monkey();
+    int id = m.generateId(23434);
+    int secondId = m.generateId(23434);
+    assertEquals(id,secondId);
+  }
+
+  @Test
+  public void negativeParameterIdTest(){
+    Monkey m = new Monkey();
+    int id = m.generateId(-1);
+    assertEquals(id,223491);
+  }
 }
