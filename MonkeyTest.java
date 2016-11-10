143e39be98a1e8c99167577349a2cfa7736fc263
Nicholas Taglianetti <Nicholas Taglianetti>
2016-11-09 19:41:15 -0500

    test added to MonkeyTest, HEADER removed from MonkeySim

--- MonkeyTest.java
+++ MonkeyTest.java
@@ -9,28 +9,36 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonkeyTest {
-//Test that generateId returns the correct ID
+  // Test that generateId returns the correct ID
   @Test
-  public void returnsCorrectId(){
+  public void testReturnsCorrectId(){
     Monkey m = new Monkey();
     int id = m.generateId(3);
     assertEquals(id,223495);
   }
 
-  //Every time generateId is ran with the same input it should return the same ID
+  // Every time generateId is ran with the same input it should return the same ID
   @Test
-  public void checkSameIdReturned(){
+  public void testCheckSameIdReturned(){
     Monkey m = new Monkey();
     int id = m.generateId(23434);
     int secondId = m.generateId(23434);
     assertEquals(id,secondId);
   }
 
-  //Test that it returns the correct ID with a negative number
+  // Test that generateId returns the correct ID with a negative input
   @Test
-  public void negativeParameterIdTest(){
+  public void testNegativeParameterIdTest(){
     Monkey m = new Monkey();
     int id = m.generateId(-1);
+    assertEquals(id,223491);
+  }
+  
+  // Test that generateId returns the correct ID with a very large input
+  @Test
+  public void testLargeParameterIdTest(){
+    Monkey m = new Monkey();
+    int id = m.generateId(2000000);
     assertEquals(id,223491);
   }
 }
